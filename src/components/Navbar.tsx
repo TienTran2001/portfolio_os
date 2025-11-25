7c4058693f142f33f07b86ad53338ca9b262e73d
tientv <tienco201@gmail.com>
2025-11-25 16:00:15 +0700

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,31 @@
+import dayjs from 'dayjs';
+import { navIcons, navLinks } from '#constants/index';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="logo" />
+        <p className="font-bold">Tien's Portfolio</p>
+        <ul>
+          {navLinks.map(({ id, name }) => (
+            <li key={id}>{name}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img src={img} alt={`icon-${id}`} className="icon-hover" />
+            </li>
+          ))}
+        </ul>
+        <time>{dayjs().format('ddd MMM D h:mm A')}</time>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
